d569d0fe15d9f44ff351cf9400258a1c8c9530f0
WordChained <talbd51@gmail.com>
2022-06-03 16:10:03 +0300

    .

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,29 +1,25 @@
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { logoutAction } from '../../../store/actions/loginActions';
-import { LoginContext } from '../../../store/contexts/LoginContext';
-import styles from './Header.module.css';
-import { useNavigate } from 'react-router';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { logoutAction } from "../../../store/actions/loginActions";
+import { LoginContext } from "../../../store/contexts/LoginContext";
+import styles from "./Header.module.css";
 export const Header = () => {
   const { loginState, loginDispatch } = useContext(LoginContext);
-  const [loginTitle, setLoginTitle] = useState('Login');
   console.log(loginState);
-  const navigate = useNavigate();
+
   const onLogout = () => {
     loginDispatch(logoutAction());
-    navigate('/login');
   };
+
   return (
     <header className={styles.headerContainer}>
       <nav>
-        <Link to='/'>Home</Link>
+        <Link to="/">Home</Link>
         <div className={styles.miniNav}>
-          <Link to='/rooms'>Rooms</Link>
-          {!loginState.user ? (
-            <Link to='/login'>Login</Link>
-          ) : (
-            <a onClick={onLogout}>Logout</a>
-          )}
+          <Link to="/rooms">Rooms</Link>
+          <Link to="/login" onClick={!loginState.user ? "" : onLogout}>
+            {!loginState.user ? "Login" : "Logout"}
+          </Link>
         </div>
       </nav>
     </header>
